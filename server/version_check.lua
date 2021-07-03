eb47d3d4c0c3ad9c3dd3f88b80a5cbb2bc690f93
Who I Am <74838696+string-null@users.noreply.github.com>
2021-07-03 20:42:34 +0200

    Update version_check.lua

--- server/version_check.lua
+++ server/version_check.lua
@@ -16,7 +16,7 @@ Citizen.CreateThread(function()
     local current_version = GetResourceMetadata(resource_name, 'version', 0)
     PerformHttpRequest('https://raw.githubusercontent.com/string-null/Nonpayer_Versions/master/'..resource_name..'.txt',function(error, result, headers)
         local new_version = result:sub(1, -2)
-        SetConvarServerInfo("np_caryped [by Juanjo#7253]", current_version)
+        SetConvarServerInfo("np_carryped [by Juanjo#7253]", current_version)
         if new_version ~= current_version then
             print('^2['..resource_name..'] - New Update Available.^0\nCurrent Version: ^5'..current_version..'^0\nNew Version: ^5'..new_version..'^0')
         end
