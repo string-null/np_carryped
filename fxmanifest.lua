2bed92ade1cdfa6037ce98d4afebe1f2ed9be95e
Who I Am <74838696+string-null@users.noreply.github.com>
2021-07-03 20:41:15 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,9 @@ fx_version 'adamant'
 
 game 'gta5'
 
-author 'Juanjo#7253 & Loonz#2090'
+author 'Juanjo#7253 & Loonz#2090 | discord.gg/nonpayer'
 description 'Simple script to Carry Peds on FiveM.'
+version '1.0.0' -- DONT TOUCH THIS
 
 client_script 'client/client.lua'
 server_script 'server/version_check.lua'
